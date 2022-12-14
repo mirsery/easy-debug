749d6aa30d4c70394a5e77d06791e3f9bcfbfbf0
mirsery <mirsery@163.com>
2022-12-14 23:07:35 +0800

    V1.0.0基础版本上线

--- src/main/java/com/mirsery/easy/page/MainFrame.java
+++ src/main/java/com/mirsery/easy/page/MainFrame.java
@@ -1,6 +1,7 @@
 package com.mirsery.easy.page;
 
 import java.awt.Dimension;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -13,12 +14,15 @@ public class MainFrame extends JFrame {
     @Resource
     private BodyJPanel bodyJPanel;
 
+    @Value("${project.version}")
+    private String version;
+
     public void init() {
         this.setMinimumSize(new Dimension(700, 480));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocation(800, 200);
 
-        this.setTitle("websocket调试工具");
+        this.setTitle("websocket调试工具 " + version);
 
         this.setResizable(true);
 
